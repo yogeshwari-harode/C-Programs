3e8ccf8b7aaddf01ac00e9ab23260fda16bffb68
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2020-11-29 22:14:48 +0530

    Create Find percentage.c

---
+++ Find percentage.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int Hindi,English,Maths,Physics,Chemistry,Percentage;
+    Printf(" Enter the marks of Hindi");
+    scanf("%d",& Hindi);
+    Printf(" Enter the marks of English);
+    scanf("%d",& English);
+    Printf(" Enter the marks of Maths);
+    scanf("%d",& Maths);
+    Printf(" Enter the marks of Physics);
+    scanf("%d",& Physics);
+    Printf(" Enter the marks of Chemistry);
+    scanf("%d",& Chemistry);
+    Percentage=(Hindi+English+Maths+Physics+Chemistry)/5;
+    printf ("percentage of student is =%d", percentage);
+    getch();
+}    
