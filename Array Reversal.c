aee025c928270b55944e4296e78f35f41e1bba61
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-02-27 15:38:53 +0530

    Create Array Reversal.c

--- Array Reversal.c
+++ Array Reversal.c
@@ -2,12 +2,14 @@
 int main()
 {
     int n;
+    printf("Enter the size of an Array : ");
     scanf("%d",&n);
     int arr[n];
     for(int i=0; i<n; i++)
         {
             scanf("%d",&arr[i]);
         }
+    printf("The reverse of the array is : ");
     for(int i=n; i>=1; i--)
           {   
             printf("%d ",arr[i-1]);
